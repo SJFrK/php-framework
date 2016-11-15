0c4b4e5e8b66c90c85ce51a7606ec6f0873009d1
Tivoh <contact@tivoh.com>
2016-11-15 11:55:59 +0100

    Save relations on Model->save().

--- src/Model.php
+++ src/Model.php
@@ -8,6 +8,7 @@ abstract class Model {
 	protected static $relations = [];
 	protected static $primaryKey = 'id';
 	protected $data;
+	protected $dirty = false;
 
 	public final function __construct(array $data = null) {
 		if (static::$table == null) {
@@ -46,10 +47,12 @@ abstract class Model {
 
 	public function __set($key, $val) {
 		$this->data[$key] = $val;
+		$this->dirty = true;
 	}
 
 	public function fromArray($data) {
 		$this->data = $data;
+		$this->dirty = true;
 	}
 
 	public function save() {
@@ -72,45 +75,51 @@ abstract class Model {
 
 		foreach (static::$fields as $field => $type) {
 			$placeholders[] = ':' . $field;
-			$fields[] = static::escape($field);
+			$fields[] = Database::sanitize($field);
 			$data[':' . $field] = $this->__get($field);
 		}
 
 		$fields = implode(', ', $fields);
 		$placeholders = implode(', ', $placeholders);
 
-		$st = $db->prepare('INSERT INTO ' . static::escape(static::$table) . ' (' . $fields . ') VALUES (' . $placeholders . ')');
+		$st = $db->prepare('INSERT INTO ' . Database::sanitize(static::$table) . ' (' . $fields . ') VALUES (' . $placeholders . ')');
 
 		return $st->execute($data);
 	}
 
 	public function update() {
+		if ($this->dirty == false) {
+			return true;
+		}
+
+		foreach (static::$relations as $field => $relation) {
+			if (array_key_exists($field, $this->data)) {
+				$this->__get($field)->save();
+			}
+		}
+
 		$db = Database::getHandle();
 
 		$data = [];
 		$fields = [];
 
 		foreach (static::$fields as $field => $type) {
-			$fields[] = static::escape($field) . ' = :' . $field;
+			$fields[] = Database::sanitize($field) . ' = :' . $field;
 			$data[':' . $field] = $this->__get($field);
 		}
 
 		$fields = implode(', ', $fields);
 
-		$st = $db->prepare('UPDATE ' . static::escape(static::$table) . ' SET ' . $fields . ' WHERE ' . static::escape(static::$primaryKey) . ' = :' . static::$primaryKey);
+		$st = $db->prepare('UPDATE ' . Database::sanitize(static::$table) . ' SET ' . $fields . ' WHERE ' . Database::sanitize(static::$primaryKey) . ' = :' . static::$primaryKey);
 
 		return $st->execute($data);
 	}
 
 	public function delete() {
 		$db = Database::getHandle();
-		$st = $db->prepare('DELETE FROM `' . str_replace('`', '', static::$table) . '` WHERE ' . static::escape(static::$primaryKey) . ' = ?');
+		$st = $db->prepare('DELETE FROM `' . str_replace('`', '', static::$table) . '` WHERE ' . Database::sanitize(static::$primaryKey) . ' = ?');
 
 		return $st->execute([$this->__get(static::$primaryKey)]);
-	}
-
-	public static function escape($text) {
-		return '`' . str_replace('`', '', $text) . '`';
 	}
 
 	public static function find($key, $val, $limit = 1) {
@@ -128,7 +137,7 @@ abstract class Model {
 			throw new \LogicException(get_called_class() . '::$primaryKey is not set.');
 		}
 
-		$query = 'SELECT * FROM ' . static::escape(static::$table) . ' WHERE ' . static::escape($key) . ' = ?';
+		$query = 'SELECT * FROM ' . Database::sanitize(static::$table) . ' WHERE ' . Database::sanitize($key) . ' = ?';
 
 		if (is_numeric($limit) && $limit >= 1) {
 			$query .= ' LIMIT ' . $limit;
